24a1f8c8412059b9d95241deb9932f7e246784c6
roxywasiak <ruksclone@hotmail.com>
2025-08-29 18:14:15 +0100

    added code to clear mocks after each test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,36 +1,32 @@
-import { render, screen, waitFor} from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
 describe('App Integration Test', () => {
-  test('renders login if no token is found', async () => {
+  afterEach(() => {
     localStorage.clear(); 
+  });
 
+  test('renders login if no token is found', async () => {
     render(<App />);
-
     expect(await screen.findByText(/Login/i)).toBeInTheDocument();
     expect(screen.getByText(/Select user/i)).toBeInTheDocument();
   });
 
   test('loads welcome message if token is present', async () => {
     localStorage.setItem('token', 'staff-token');
-
     render(<App />);
-
     await waitFor(() =>
-      expect(
-        screen.getByText(/Welcome, Alice \(staff\)/i)
-      ).toBeInTheDocument()
+      expect(screen.getByText(/Welcome, Alice \(staff\)/i)).toBeInTheDocument()
     );
   });
 
   test('shows unauthorized if token is invalid', async () => {
     localStorage.setItem('token', 'invalid-token');
-
     render(<App />);
-
     await waitFor(() =>
       expect(screen.getByText(/Login/i)).toBeInTheDocument()
     );
   });
 });
+
 
